094eea090035cb8efe44e5655c4d575222ddbe6d
abui <adjiem31@gmail.com>
2024-04-03 23:54:05 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"gihub.com/abui-am/tubes-rpl/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
